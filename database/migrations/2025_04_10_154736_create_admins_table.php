b4eefb6baa8101729e422326bd67137a7cd80ca1
Ananya Singh <singhananya5040@gmail.com>
2025-04-25 19:58:57 +0530

    migration

--- database/migrations/2025_04_10_154736_create_admins_table.php
+++ database/migrations/2025_04_10_154736_create_admins_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 return new class extends Migration
 {
@@ -18,6 +20,14 @@ return new class extends Migration
             $table->string('password');
             $table->timestamps();
         });
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password123'),
+            'is_admin' => true,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
         
     }
 
